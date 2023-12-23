da7be4ecb973a81ca98aa6705206e23c2e44c307
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2023-12-23 11:48:55 +0100

    include src code in containers volume

--- src/app.py
+++ src/app.py
@@ -1,4 +1,20 @@
 import sys
+from pymongo import MongoClient
+
+
+def connect_to_mongodb():
+    client = MongoClient("mongodb://mongodb:27017/")
+    db = client["dictionary-ca"]
+    return db
+
+
+def query_word(word):
+    db = connect_to_mongodb()
+    collection = db["words"]
+
+    result = collection.find_one({"word": word})
+    return result
+
 
 if __name__ == "__main__":
 
@@ -7,4 +23,9 @@ if __name__ == "__main__":
         sys.exit(1)
 
     word = sys.argv[1]
-    print(f"Word is {word}")
+    result = query_word(word)
+
+    if result:
+        print(f"Word {word} is in the dictionary")
+    else:
+        print(f"Word {word} is NOT in the dictionary")
